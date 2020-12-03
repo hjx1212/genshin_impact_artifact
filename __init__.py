5fd0130a7888bcb1559f06ea38b73354ec9e57aa
hjx1212 <hjx2221@gmail.com>
2020-12-03 21:58:56 +0800

    重构常量, 适配词条出现概率

--- __init__.py
+++ __init__.py
@@ -4,226 +4,86 @@ from io import BytesIO
 from random import randrange, choice, choices, random
 from PIL import Image, ImageFont, ImageDraw, ImageMath
 
-from hoshino import Service
-sv = Service('原神圣遗物')
+from .artifact_data import *
 
+from hoshino import Service
+
+sv = Service('原神圣遗物')
 
 AUTO_ENHANCE = True
 
-suit = [
-    # 0
-    ("平息鸣雷的尊者", ("平雷之心", "平雷之羽", "平雷之刻", "平雷之器", "平雷之冠"),
-     ("雷元素抗性提高40%。", "对处于雷元素影响下的敌人造成的伤害提升35%。"),
-     ("平雷套", "平雷")),
-    # 1
-    ("渡过烈火的贤人", ("渡火者的决绝", "渡火者的解脱", "渡火者的煎熬", "渡火者的醒悟", "渡火者的智慧"),
-     ("火元素抗性提高40%。", "对处于火元素影响下的敌人造成的伤害提升35%。"),
-     ("渡火者套", "渡火套", "渡火者", "渡火")),
-    # 2
-    ("被怜爱的少女", ("远方的少女之心", "少女飘摇的思念", "少女苦短的良辰", "少女片刻的闲暇", "少女易逝的芳颜"),
-     ("角色造成的治疗效果提升15%。", "施放元素战技或元素爆发后的10秒内，队伍中所有角色受治疗效果加成提高20%。"),
-     ("少女套", "治疗套", "少女")),
-    # 3
-    ("角斗士的终幕礼", ("角斗士的留恋", "角斗士的归宿", "角斗士的希冀", "角斗士的酣醉", "角斗士的凯旋"),
-     ("攻击力提高18%。", "装备该圣遗物套装的角色为单手剑、双手剑、长柄武器角色时，角色普通攻击造成的伤害提高35%。"),
-     ("角斗士套", "角斗士")),
-    # 4
-    ("翠绿之影", ("野花记忆的绿野", "猎人青翠的箭羽", "翠绿猎人的笃定", "翠绿猎人的容器", "翠绿的猎人之冠"),
-     ("获得15%风元素伤害加成。", "扩散反应造成的伤害提升60%。根据扩散的元素类型，降低受到影响的敌人40%的对应元素抗性，持续10秒。"),
-     ("翠绿套", "风套", "翠绿")),
-    # 5
-    ("流浪大地的乐团", ("乐团的晨光", "琴师的箭羽", "终幕的时计", "吟游者之壶", "指挥的礼帽"),
-     ("元素精通提高80点。", "装备该圣遗物套装的角色为法器、弓箭角色时，角色重击造成的伤害提高35%。"),
-     ("流浪大地套", "流浪大地")),
-    # 6
-    ("如雷的盛怒", ("雷鸟的怜悯", "雷灾的孑遗", "雷霆的时计", "降雷的凶兆", "唤雷的头冠"),
-     ("获得15%雷元素伤害加成。", "超载、感电、超导反应造成的伤害提升40%。触发这些元素反应时，元素战技冷却时间减少1秒。该效果每0.8秒最多触发一次。"),
-     ("如雷套", "雷套", "如雷")),
-    # 7
-    ("炽烈的炎之魔女", ("魔女的炎之花", "魔女常燃之羽", "魔女破灭之时", "魔女的心之火", "焦灼的魔女帽"),
-     ("获得15%火元素伤害加成。", "超载、燃烧反应造成的伤害提升40%，蒸发、融化反应的加成系数提高15%。施放元素战技后的10秒内，2件套的效果提高50%，该效果最多叠加3次。"),
-     ("魔女套", "火套", "魔女")),
-    # 8
-    ("昔日宗室之仪", ("宗室之花", "宗室之翎", "宗室时计", "宗室银瓮", "宗室面具"),
-     ("元素爆发造成的伤害提升20％。", "施放元素爆发后，队伍中所有角色攻击力提升20％，持续12秒。该效果不可叠加。"),
-     ("宗室套", "宗室")),
-    # 9
-    ("染血的骑士道", ("染血的铁之心", "染血的黑之羽", "骑士染血之时", "染血骑士之杯", "染血的铁假面"),
-     ("造成的物理伤害提高25%。", "击败敌人后的10秒内，施放重击时不消耗体力，且造成的伤害提升50%。"),
-     ("染血套", "骑士套", "物理套", "染血", "骑士")),
-    # 10
-    ("悠古的磐岩", ("磐陀裂生之花", "嵯峨群峰之翼", "星罗圭壁之晷", "巉岩琢塑之樽", "不动玄石之相"),
-     ("获得15%岩元素伤害加成。", "获得岩元素反应形成的晶片时，队伍中所有角色获得35%对应元素伤害加成，持续10秒。同时只能通过该效果获得一种元素伤害加成。"),
-     ("磐岩套", "岩套", "磐岩")),
-    # 11
-    ("逆飞的流星", ("夏祭之花", "夏祭终末", "夏祭之刻", "夏祭水玉", "夏祭之面"),
-     ("护盾强效提高35%。", "处于护盾庇护下时，额外获得40%普通攻击和重击伤害加成。"),
-     ("流星套", "夏祭套", "流星", "夏祭")),
-    # 12
-    ("征服寒冬的勇士", ("碎冰者的思念", "碎冰者的自矜", "碎冰者的终期", "碎冰者的傲骨", "碎冰者的骄傲"),
-     ("冰元素抗性提高40%。", "对处于冻结状态及冰元素影响下的敌人造成的伤害提升35%。"),
-     ("寒冬套", "碎冰者套", "碎冰套", "寒冬", "碎冰者", "碎冰")),
-    # 13
-    ("冰之川与雪之砂", ("凛冬霜心", "雪藏之羽", "凝冰成砂", "北风之盏", "冰河之冠"),
-     ("获得15%冰元素伤害加成。", "超导反应造成的伤害提升50%，融化反应加成系数提高15%，施放元素爆发后的10秒内，冰元素伤害加成额外提升25%。"),
-     ("冰雪套", "冰套", "冰雪")),
-]
-slot = [
-    ("生之花", ("花",)),
-    ("死之羽", ("羽", "羽毛")),
-    ("时之沙", ("沙", "沙漏")),
-    ("空之杯", ("杯", "杯子")),
-    ("理之冠", ("冠", "头", "帽子", "帽")),
-]
-
-attrs = [
-    # 0
-    ("生命值", 717, 4780, (209, 239, 269, 299),
-     ()),
-    # 1
-    ("生命值", .070, .466, (.041, .047, .053, .058),
-     ("生命", "生", "血")),
-    # 2
-    ("攻击力", 47, 311, (14, 16, 18, 19),
-     ()),
-    # 3
-    ("攻击力", .070, .466, (.041, .047, .053, .058),
-     ("攻击", "攻")),
-    # 4
-    ("防御力", None, None, (16, 19, 21, 23),
-     ()),
-    # 5
-    ("防御力", .087, .583, (.051, .058, .066, .073),
-     ("防御", "防")),
-    # 6
-    ("元素精通", 28, 187, (16, 19, 21, 23),
-     ("元素", "精通")),
-    # 7
-    ("元素充能效率", .078, .518, (.045, .052, .058, .065),
-     ("元素充能", "充能效率", "充能")),
-    # 8
-    ("暴击率", .047, .311, (.027, .031, .035, .039),
-     ("暴击", "暴率", "暴")),
-    # 9
-    ("暴击伤害", .093, .622, (.054, .062, .070, .078),
-     ("暴伤",)),
-    # 10
-    ("物理伤害加成", .087, .583, None,
-     ("物理", "物", "物伤")),
-    # 11
-    ("火元素伤害加成", .070, .466, None,
-     ("火元素", "火", "火伤")),
-    # 12
-    ("水元素伤害加成", .070, .466, None,
-     ("水元素", "水", "水伤")),
-    # 13
-    ("雷元素伤害加成", .070, .466, None,
-     ("雷元素", "雷", "雷伤")),
-    # 14
-    ("冰元素伤害加成", .070, .466, None,
-     ("冰元素", "冰", "冰伤")),
-    # 15
-    ("风元素伤害加成", .070, .466, None,
-     ("风元素", "风", "风伤")),
-    # 16
-    ("岩元素伤害加成", .070, .466, None,
-     ("岩元素", "岩", "岩伤")),
-    # 17
-    ("治疗加成", .054, .359, None,
-     ("治疗",)),
-]
-
-available_main_attrs = [(0,), (2,), (1, 3, 5, 6, 7), (1, 3, 5, 6, 10, 11, 12, 13, 14, 15, 16), (1, 3, 5, 6, 8, 9, 17)]
-available_sub_attrs = [(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)] * 5
-
-max_attr = 4
-attr_rate = (1., 1., 1., .2, .0)
-sub_attr_value_weight = (10, 35, 35, 20)
-
-dungeon = [
-    ("精英级怪物,BOSS级怪物", (3, 5),  # 角斗士的终幕礼, 流浪大地的乐团
-     ("精英", "BOSS", "无相", "龙", "狼")),
-    ("无妄引咎密宫：祝圣秘境：寒霜", (1, 7),  # 渡过烈火的贤人, 炽烈的炎之魔女
-     ("无妄引咎密宫", "寒霜", "火本")),
-    ("仲夏庭园：祝圣秘境：净化之炎", (0, 6),  # 平息鸣雷的尊者, 如雷的盛怒
-     ("仲夏庭园", "净化之炎", "雷本")),
-    ("孤云凌霄之处：祝圣秘境：惊蛰", (10, 11),  # 悠古的磐岩, 逆飞的流星
-     ("孤云凌霄之处", "惊蛰", "岩本")),
-    ("铭记之谷：祝圣秘境：钢铁之舞", (2, 4),  # 被怜爱的少女, 翠绿之影
-     ("铭记之谷", "钢铁之舞", "风本", "少女本", "治疗本")),
-    ("华池岩岫：祝圣秘境：岩牢", (9, 8),  # 染血的骑士道, 昔日宗室之仪
-     ("华池岩岫", "岩牢", "物理本")),
-    ("???????????????", (12, 13),  # 征服寒冬的勇士, 冰之川与雪之砂
-     ("冰本",))
-]
+for v in slot.values():
+    v["main"] = [k for k, c in v["main_weight"].items() for _ in range(c)]
+    v["sub"] = [k for k, c in v["sub_weight"].items() for _ in range(c)]
 
 keyword_max_len = 10
 keywords = {}
-for i, v in enumerate(suit):
-    keywords[v[0]] = {"suit": i}
-    for ii, vv in enumerate(v[1]):
-        keywords[vv] = {"suit": i, "slot": ii}
-    for vv in v[-1]:
-        keywords[vv] = {"suit": i}
-for i, v in enumerate(slot):
-    keywords[v[0]] = {"slot": i}
-    for vv in v[-1]:
-        keywords[vv] = {"slot": i}
-for i, v in enumerate(attrs):
-    keywords[v[0]] = {"main_attr": i}
-    for vv in v[-1]:
-        keywords[vv] = {"main_attr": i}
-for i, v in enumerate(dungeon):
-    for vv in v[-1]:
-        keywords[vv] = {"dungeon": i}
+for k, v in suit.items():
+    keywords[k] = {"suit": k}
+    for kk in slot.keys():
+        keywords[v[kk]] = {"suit": k, "slot": kk}
+    for vv in v["alias"]:
+        keywords[vv] = {"suit": k}
+for k, v in slot.items():
+    keywords[k] = {"slot": k}
+    for vv in v["alias"]:
+        keywords[vv] = {"slot": k}
+for k, v in attrs.items():
+    keywords[k] = {"main_attr": k}
+    for vv in v["alias"]:
+        keywords[vv] = {"main_attr": k}
+for k, v in dungeon.items():
+    for vv in v["alias"]:
+        keywords[vv] = {"dungeon": k}
 
 RES_PATH = os.path.join(os.path.dirname(__file__), 'res')
-background = Image.open(os.path.join(RES_PATH, 'background.png'))
+background = Image.open(os.path.join(RES_PATH, 'background.png')).resize((550, 570))
 ttf_path = os.path.join(RES_PATH, "zh-cn.ttf")
 
 
 def enhance_sub_attr(artifact):
     if len(artifact["sub_attr"]) < max_attr:
         while True:
-            attr_id = choice(available_sub_attrs[artifact["slot"]])
-            if artifact["main_attr"]["id"] == attr_id:
+            attr_name = choice(slot[artifact["slot"]]["sub"])
+            if artifact["main_attr"]["name"] == attr_name:
                 continue
             for sub_attr in artifact["sub_attr"]:
-                if sub_attr["id"] == attr_id:
-                    attr_id = -1
+                if sub_attr["name"] == attr_name:
+                    attr_name = None
                     break
-            if attr_id < 0:
+            if not attr_name:
                 continue
-            attr_value = choices(attrs[attr_id][3], sub_attr_value_weight)[0]
-            artifact["sub_attr"].append({"id": attr_id, "value": attr_value})
+            attr_value = choices(attrs[attr_name]["sub"], sub_attr_value_weight)[0]
+            artifact["sub_attr"].append({"name": attr_name, "value": attr_value})
             break
     else:
         sub_attr = choice(artifact["sub_attr"])
-        attr_id = sub_attr["id"]
-        attr_value = choices(attrs[attr_id][3], sub_attr_value_weight)[0]
+        attr_name = sub_attr["name"]
+        attr_value = choices(attrs[attr_name]["sub"], sub_attr_value_weight)[0]
         sub_attr["value"] += attr_value
-    return attr_id, attr_value
+    return attr_name, attr_value
 
 
 def enhance(artifact):
     ret = '强化:'
     while artifact["level"] < 20:
         artifact["level"] = (artifact["level"] // 4 + 1) * 4
-        artifact["main_attr"]["value"] = attrs[artifact["main_attr"]["id"]][2]
-        attr_id, attr_value = enhance_sub_attr(artifact)
-        ret += f' {attrs[attr_id][0]}'
+        artifact["main_attr"]["value"] = attrs[artifact["main_attr"]["name"]]["main_20"]
+        attr_name, attr_value = enhance_sub_attr(artifact)
+        ret += f' {attrs[attr_name]["name"]}+{print_value(attr_value)}'
     return ret
 
 
-def get_rand_artifact(suit_id=None):
-    suit_id = suit_id if isinstance(suit_id, int) else choice(suit_id) if suit_id else randrange(len(suit))
-    slot_id = randrange(len(slot))
-    main_attr_id = choice(available_main_attrs[slot_id])
+def get_rand_artifact(suit_name=None):
+    suit_name = suit_name if isinstance(suit_name, str) else choice(suit_name) if suit_name else choice(
+        list(suit.keys()))
+    slot_name = choice(list(slot.keys()))
+    main_attr = choice(slot[slot_name]["main"])
     artifact = {
-        "suit": suit_id,
-        "slot": slot_id,
+        "suit": suit_name,
+        "slot": slot_name,
         "level": 0,
-        "main_attr": {"id": main_attr_id, "value": attrs[main_attr_id][1]},
+        "main_attr": {"name": main_attr, "value": attrs[main_attr]["main_0"]},
         "sub_attr": [],
     }
     attr_p = random()
@@ -232,16 +92,23 @@ def get_rand_artifact(suit_id=None):
     return artifact
 
 
+def print_value(value):
+    return f'{value:{"d" if isinstance(value, int) else ".1%"}}'
+
+
+def print_attr(attr):
+    return f'{attrs[attr["name"]]["name"]}+{print_value(attr["value"])}'
+
+
 def print_artifact(artifact):
     ret = ''
-    ret += f'{suit[artifact["suit"]][1][artifact["slot"]]} +{artifact["level"]}\n'
-    ret += f'{slot[artifact["slot"]][0]}\n'
-    attr = artifact["main_attr"]
-    ret += f'{attrs[attr["id"]][0]}+{attr["value"]:{"d" if isinstance(attr["value"], int) else ".1%"}}\n'
+    ret += f'{suit[artifact["suit"]][artifact["slot"]]} +{artifact["level"]}\n'
+    ret += f'{artifact["slot"]}\n'
+    ret += f'{print_attr(artifact["main_attr"])}\n'
     ret += f'★★★★★\n'
     for attr in artifact["sub_attr"]:
-        ret += f'· {attrs[attr["id"]][0]}+{attr["value"]:{"d" if isinstance(attr["value"], int) else ".1%"}}\n'
-    ret += f'{suit[artifact["suit"]][0]}'
+        ret += f'· {print_attr(attr)}\n'
+    ret += f'{artifact["suit"]}'
     return ret
 
 
@@ -250,29 +117,26 @@ def get_artifact_image(artifact):
 
     img = background.copy()
 
-    name = suit[artifact["suit"]][1][artifact["slot"]]
+    name = suit[artifact["suit"]][artifact["slot"]]
     icon = Image.open(os.path.join(RES_PATH, name + '.png'))
-    icon = icon.resize((190, 190))
+    # icon = icon.resize((190, 190))
     # icon_a = icon.getchannel("A")  # 有的图alpha通道有问题，需要对alpha处理一下
     # icon_a = ImageMath.eval("convert(a*b/256, 'L')", a=icon_a, b=icon_a)
-    img.paste(icon, (218, 48), icon)
+    img.paste(icon, (292, 64), icon)
 
     draw = ImageDraw.Draw(img)
-    draw.text((24, 8), name, fill="#ffffff", font=ImageFont.truetype(ttf_path, size=28))
-    draw.text((24, 59), slot[artifact["slot"]][0], fill="#ffffff", font=ImageFont.truetype(ttf_path, size=17))
+    draw.text((32, 10), name, fill="#ffffff", font=ImageFont.truetype(ttf_path, size=37))
+    draw.text((32, 79), artifact["slot"], fill="#ffffff", font=ImageFont.truetype(ttf_path, size=23))
     attr = artifact["main_attr"]
-    draw.text((24, 128), attrs[attr["id"]][0], fill="#bfafa8", font=ImageFont.truetype(ttf_path, size=17))
-    draw.text((24, 148), f'{attr["value"]:{"d" if isinstance(attr["value"], int) else ".1%"}}',
-              fill="#ffffff", font=ImageFont.truetype(ttf_path, size=35))
+    draw.text((32, 171), attrs[attr["name"]]["name"], fill="#bfafa8", font=ImageFont.truetype(ttf_path, size=23))
+    draw.text((32, 198), print_value(attr["value"]), fill="#ffffff", font=ImageFont.truetype(ttf_path, size=47))
     level = f'+{artifact["level"]}'
-    w, h = draw.textsize(level, font=ImageFont.truetype(ttf_path, size=20))
-    draw.text((50 - w / 2, 269 - h / 2), level, fill="#ffffff", font=ImageFont.truetype(ttf_path, size=20))
-    x = 24
-    y = 300
+    w, h = draw.textsize(level, font=ImageFont.truetype(ttf_path, size=27))
+    draw.text((64 - w / 2, 360 - h / 2), level, fill="#ffffff", font=ImageFont.truetype(ttf_path, size=27))
+    x, y = 32, 401
     for attr in artifact["sub_attr"]:
-        draw.text((x, y), f'·{attrs[attr["id"]][0]}+{attr["value"]:{"d" if isinstance(attr["value"], int) else ".1%"}}',
-                  fill="#495366", font=ImageFont.truetype(ttf_path, size=20))
-        y += 32
+        draw.text((x, y), f'·{print_attr(attr)}', fill="#495366", font=ImageFont.truetype(ttf_path, size=27))
+        y += 43
 
     return img
 
@@ -303,22 +167,42 @@ def parse_content(content):
             raise ValueError(f'没有找到关键词: {content}')
 
     if target["suit"]:
-        dungeon_id = None
-        for i, v in enumerate(dungeon):
-            if target["suit"] in v[1]:
-                dungeon_id = i
+        dungeon_name = None
+        for k, v in dungeon.items():
+            if target["suit"] in v["reward"]:
+                dungeon_name = k
                 break
         if not target["dungeon"]:
-            target["dungeon"] = dungeon_id
-        elif target["dungeon"] != dungeon_id:
+            target["dungeon"] = dungeon_name
+        elif target["dungeon"] != dungeon_name:
             raise ValueError(f'圣遗物套装与所刷取的秘境不符')
     if target["slot"] and target["main_attr"]:
-        if target["slot"] in (0, 1):
-            target["main_attr"] -= 1  # trick...
-        if target["main_attr"] not in available_main_attrs[target["slot"]]:
+        if target["slot"] in ("生之花", "死之羽") and len(target["main_attr"]) > 3:
+            target["main_attr"] = target["main_attr"][:-3]  # trick...
+        if target["main_attr"] not in slot[target["slot"]]["main"]:
             raise ValueError(f'圣遗物位置的主属性不符')
 
     return target
+
+
+def get_target_artifact(target, max_times=10000):
+    ok = False
+    for times in range(max(max_times, 1)):
+        artifact = get_rand_artifact(dungeon[target["dungeon"]]["reward"] if target["dungeon"] else None)
+        if target["suit"] and target["suit"] != artifact["suit"]:
+            continue
+        if target["slot"] and target["slot"] != artifact["slot"]:
+            continue
+        if target["main_attr"] and target["main_attr"] != artifact["main_attr"]["name"]:
+            continue
+        ok = True
+        break
+    msg = f'刷取{target["dungeon"] if target["dungeon"] else ""}圣遗物{times + 1}次，'
+    if not ok:
+        msg += '还是没有获得指定圣遗物，你脸也太黑了吧，拿着这个，凑合着用吧：\n'
+    else:
+        msg += '获得：\n'
+    return artifact, msg
 
 
 @sv.on_prefix(["圣遗物", "原神圣遗物"], only_to_me=True)
@@ -328,33 +212,17 @@ async def genshin_artifact(bot, ev):
     except Exception as e:
         await bot.send(ev, str(e))
         return
-    target_dungeon = dungeon[target["dungeon"]] if target["dungeon"] else ("", None)
-    ok = False
-    for times in range(10000):
-        artifact = get_rand_artifact(target_dungeon[1])
-        if target["suit"] and target["suit"] != artifact["suit"]:
-            continue
-        if target["slot"] and target["slot"] != artifact["slot"]:
-            continue
-        if target["main_attr"] and target["main_attr"] != artifact["main_attr"]["id"]:
-            continue
-        ok = True
-        break
-    msg = f'刷取{target_dungeon[0]}圣遗物{times + 1}次，'
-    if not ok:
-        msg += '还是没有获得指定圣遗物，你脸也太黑了吧，拿着这个，凑合着用吧：'
-    else:
-        msg += '获得：'
+    artifact, msg = get_target_artifact(target)
     try:
         msg += print_artifact_img_CQ(artifact)
     except:
-        msg += '\n' + print_artifact(artifact) + '\n'
+        msg += print_artifact(artifact)
     if AUTO_ENHANCE:
-        msg += '\n' + enhance(artifact)
+        msg += f'\n\n{enhance(artifact)}\n'
         try:
             msg += print_artifact_img_CQ(artifact)
         except:
-            msg += '\n' + print_artifact(artifact)
+            msg += print_artifact(artifact)
     await bot.send(ev, msg, at_sender=True)
 
 
@@ -362,8 +230,8 @@ if __name__ == '__main__':
     # main()
 
     test = {
-        "suit": 0, "slot": 0, "level": 0, "main_attr": {"id": 0, "value": 717},
-        "sub_attr": [{"id": 6, "value": 19}, {"id": 8, "value": .035}, {"id": 9, "value": .078}],
+        "suit": "平息鸣雷的尊者", "slot": "生之花", "level": 0, "main_attr": {"name": "生命值", "value": 717},
+        "sub_attr": [{"name": "元素精通", "value": 19}, {"name": "暴击率", "value": .035}, {"name": "暴击伤害", "value": .078}],
     }
     print(print_artifact(test))
     enhance(test)
@@ -371,6 +239,13 @@ if __name__ == '__main__':
 
     test = get_rand_artifact()
     print(print_artifact(test))
+
+    target = parse_content("魔女火伤杯")
+    print(target)
+    test, msg = get_target_artifact(target)
+    print(msg)
+    print(print_artifact(test))
+
     enhance(test)
     img = get_artifact_image(test)
     img.show()
